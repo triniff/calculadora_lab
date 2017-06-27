9dd8718463871f77d66f0d062ef861a06ea1c465
“Trinidad <tfuentesfuentealba@gmail.com>
2017-06-27 20:42:48 -0300

    agregando jquery

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$("input:button").click(function(){
+		btn = $(this).val();
+		pantalla = $("#resultado").val();
+		if (btn=="C") {
+			$("#resultado").val("");
+		}else{
+			if (btn=="=") {
+				$("#resultado").val(eval(pantalla));
+			}else{
+				$("#resultado").val(pantalla + btn);
+			}
+		}
+	})
+
+});
